4ca9093652e534c57accfa24e6e1e3460affba7e
Marisa Vitale <marisa.vitale2306@gmail.com>
2021-01-28 12:00:28 +0100

    deploy

---
+++ src/components/layout/Footer/Footer.js
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+  return (
+    <footer className="page-footer mt-5">
+      <hr></hr>
+      <p className="text-center py-3">
+        Developed by <strong><a href="https://github.com/Joseacasado" target="_blank" rel="noreferrer"> Jose A. Casado </a></strong>
+        and <strong><a href="https://github.com/marisa2306" target="_blank" rel="noreferrer">Marisa Vitale </a></strong>
+        within <strong> Ironack </strong><em> Web Development Bootcamp</em>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
